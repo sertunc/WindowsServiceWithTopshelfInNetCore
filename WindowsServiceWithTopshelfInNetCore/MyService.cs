b5f316c5fa4a270be8734c6c0ab43f6a3b1cc8ca
cahitsertunc.selen <cahitsertunc.selen@CSSELEN>
2019-05-10 09:24:08 +0300

    Add project files.

---
+++ WindowsServiceWithTopshelfInNetCore/MyService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace WindowsServiceWithTopshelfInNetCore
+{
+    public class MyService
+    {
+        public void Dispose()
+        {
+
+        }
+
+        public bool Start()
+        {
+            var text = $"{DateTime.Now.ToString("yyyy-MM-dd HH: mm: ss")}, Testing write." + Environment.NewLine;
+
+            File.WriteAllText(@"D:\Service.Write.txt", text);
+
+            Console.WriteLine($"[{nameof(MyService)}] has been started.....");
+
+            return true;
+        }
+
+        public bool Stop()
+        {
+            //File.Delete(@"C:\ApiAgentPublish\Service.Write.txt");
+            Console.WriteLine($"[{nameof(MyService)}] has been stopped.....");
+
+            return true;
+        }
+    }
+}
---
+++ WindowsServiceWithTopshelfInNetCore/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Topshelf;
+
+namespace WindowsServiceWithTopshelfInNetCore
+{
+    // Installation
+    // go to publish folder and open your command prompt as Administrator
+    // WindowsServiceWithTopshelfInNetCore.exe install -servicename "ApiAgent" -displayname "Api Agent Display Name" -description "Api Agent Description"
+
+    // Unistall
+    // WindowsServiceWithTopshelfInNetCore.exe uninstall
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rc = HostFactory.Run(host =>
+            {
+                host.Service<MyService>(s =>
+                {
+                    s.ConstructUsing(x => new MyService());
+                    s.WhenStarted(x => x.Start());
+                    s.WhenStopped(x => x.Stop());
+                });
+
+                host.RunAsLocalSystem();
+                host.StartAutomatically();
+
+                host.SetServiceName("MyService");
+            });
+
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());
+            Environment.ExitCode = exitCode;
+        }
+    }
+}
