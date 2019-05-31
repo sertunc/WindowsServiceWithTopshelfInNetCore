087b36b261d01a2782c55500800b725c7a7f1ada
Sertunc SELEN <sertuncselen@yahoo.com>
2019-05-31 13:49:53 +0300

    Update Program.cs

--- WindowsServiceWithTopshelfInNetCore/Program.cs
+++ WindowsServiceWithTopshelfInNetCore/Program.cs
@@ -7,7 +7,7 @@ namespace WindowsServiceWithTopshelfInNetCore
     // go to publish folder and open your command prompt as Administrator
     // WindowsServiceWithTopshelfInNetCore.exe install -servicename "ApiAgent" -displayname "Api Agent Display Name" -description "Api Agent Description"
 
-    // Unistall
+    // Uninstall
     // WindowsServiceWithTopshelfInNetCore.exe uninstall
 
     class Program
